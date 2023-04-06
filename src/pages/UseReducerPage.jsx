875ca0264fcdc17c9a8b56077078de63a1497d2d
Simon Muia <simonmuia38@gmail.com>
2023-04-07 00:17:49 +0300

    Added useReducer function feature

--- src/pages/UseReducerPage.jsx
+++ src/pages/UseReducerPage.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+/**
+ * Alters two different actions: increment value and value of 
+ * text visibility.
+ *
+ */
+const reducer = (state, action) =>{
+  switch (action.type) {
+    case "INCREMENT":
+      return {count: state.count + 1, showText: state.showText}
+    case "toggleShowText":
+      return {count: state.count, showText: !state.showText}
+  
+    default:
+      return state;
+  }
+}
+
 
 const UseReducerPage = () => {
+
+  const [state, dispatch] = useReducer(reducer, 
+    {count: 0, showText:true}
+    )
+
   return (
 	<div className='wrapper'>
       <h2>UseReducer</h2>
+      <p>Uses to handle different states of a program</p>
 
       <div className='inputForm'>
-
+        <h2>{state.count}</h2>
+        <button
+        onClick={()=>{
+          dispatch({type:"INCREMENT"});
+          dispatch({type:"toggleShowText"})
+        }}        
+        >
+          Click Here
+        </button>
+        {state.showText && <p>This is a text</p>}
 		</div>
        
     </div>
